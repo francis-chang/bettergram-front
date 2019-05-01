c484e4a761b9e2a897397d4fa37398390668e373
francis <fc373745@gmail.com>
2019-05-01 02:33:10 -0700

    start

---
+++ src/Landing/Login.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+interface Props {}
+
+export const Login: React.FC<Props> = () => {
+    return (
+        <div>
+            <form>
+                <input />
+            </form>
+        </div>
+    );
+};
