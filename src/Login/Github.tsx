cd983dff3b5d830ac6a03e68c03add613afb8530
francis <fc373745@gmail.com>
2019-05-03 07:01:09 -0700

    github auth doesn't allow cross origin, so spin up a server or use fckin prose/gatekeeper

--- src/Login/Github.tsx
+++ src/Login/Github.tsx
@@ -1,7 +1,19 @@
+import axios from "axios";
 import * as React from "react";
 
 interface Props {}
 
-export const Github: React.FC<Props> = () => {
+export const Github: React.FC<Props> = (props: any) => {
+    React.useEffect(() => {
+        const params = new URLSearchParams(props.location.search);
+        const code = params.get("code");
+        axios
+            .get(
+                `http://localhost:5000/login/github/authorized?code=b015604568ef32ebe060`
+            )
+            .then(res => {
+                console.log(res.data);
+            });
+    }, []);
     return <div>hi</div>;
 };
