fb8f17897ead0a31b787892cca1b46141f8f82a7
francis <fc373745@gmail.com>
2019-05-29 03:48:13 -0700

    asdf

--- src/Landing/Landing.tsx
+++ src/Landing/Landing.tsx
@@ -1,11 +1,36 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faGithub, faGoogle, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import {
+    faGithub,
+    faGoogle,
+    faInstagram,
+    faTwitter
+} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import * as React from "react";
 import { RouteComponentProps, withRouter } from "react-router";
 import { animated, useSpring } from "react-spring";
-import { BotMessage, Container, GithubLogin, GithubLogo, GithubText, GoogleLogin, GoogleLogo, Input, Login, LoginContainer, LoginForm, SignUpToggle, SquareOne, SubmitBtn, Title, TitleText, TitleTitle, TopErrorMessage, TopMessage } from "./LandingStyles";
+import {
+    BotMessage,
+    Container,
+    GithubLogin,
+    GithubLogo,
+    GithubText,
+    GoogleLogin,
+    GoogleLogo,
+    Input,
+    Login,
+    LoginContainer,
+    LoginForm,
+    SignUpToggle,
+    SquareOne,
+    SubmitBtn,
+    Title,
+    TitleText,
+    TitleTitle,
+    TopErrorMessage,
+    TopMessage
+} from "./LandingStyles";
 
 library.add(faGithub, faInstagram, faGoogle, faTwitter);
 
@@ -168,7 +193,6 @@ const Landing: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
             <Login>
                 <LoginContainer>
                     <animated.div style={animateForm}>
-                        
                         <animated.div style={animatedBars} />
                         <SquareOne />
                         <LoginForm onSubmit={onSubmit}>
